7da299bc9dd503a1371126c03cde91563b260fc5
Sergio Moreno <sergiomorenoval@gmail.com>
2025-03-03 18:40:45 -0600

    feat: Agregada traduccion. - Verificacion de email - Cambiar contraseña - Olvide mi contraseña

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -22,8 +22,8 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
-        <Head title="Forgot password" />
+    <AuthLayout :title="$t('Forgot password')" :description="$t('Enter your email to receive a password reset link')">
+        <Head :title="$t('Forgot password')" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
@@ -32,22 +32,22 @@ const submit = () => {
         <div class="space-y-6">
             <form @submit.prevent="submit">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
+                    <Label for="email">{{ $t('Email Address') }}</Label>
+                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus />
                     <InputError :message="form.errors.email" />
                 </div>
 
                 <div class="my-6 flex items-center justify-start">
                     <Button class="w-full" :disabled="form.processing">
                         <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                        Email password reset link
+                        {{ $t('Send reset link') }}
                     </Button>
                 </div>
             </form>
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
-                <span>Or, return to</span>
-                <TextLink :href="route('login')">log in</TextLink>
+                <span>{{ $t('Or, return to') }}</span>
+                <TextLink :href="route('login')">{{ $t('Log In') }}</TextLink>
             </div>
         </div>
     </AuthLayout>
--- resources/js/pages/auth/ResetPassword.vue
+++ resources/js/pages/auth/ResetPassword.vue
@@ -31,19 +31,19 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthLayout title="Reset password" description="Please enter your new password below">
-        <Head title="Reset password" />
+    <AuthLayout :title="$t('Reset Password')" :description="$t('Please enter your new password below')">
+        <Head :title="$t('Reset Password')" />
 
         <form @submit.prevent="submit">
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="email">Email</Label>
+                    <Label for="email">{{ $t('Email Address') }}</Label>
                     <Input id="email" type="email" name="email" autocomplete="email" v-model="form.email" class="mt-1 block w-full" readonly />
                     <InputError :message="form.errors.email" class="mt-2" />
                 </div>
 
                 <div class="grid gap-2">
-                    <Label for="password">Password</Label>
+                    <Label for="password">{{ $t('Password') }}</Label>
                     <Input
                         id="password"
                         type="password"
@@ -52,13 +52,12 @@ const submit = () => {
                         v-model="form.password"
                         class="mt-1 block w-full"
                         autofocus
-                        placeholder="Password"
                     />
                     <InputError :message="form.errors.password" />
                 </div>
 
                 <div class="grid gap-2">
-                    <Label for="password_confirmation"> Confirm Password </Label>
+                    <Label for="password_confirmation">{{ $t('Confirm Password') }}</Label>
                     <Input
                         id="password_confirmation"
                         type="password"
@@ -66,14 +65,13 @@ const submit = () => {
                         autocomplete="new-password"
                         v-model="form.password_confirmation"
                         class="mt-1 block w-full"
-                        placeholder="Confirm password"
                     />
                     <InputError :message="form.errors.password_confirmation" />
                 </div>
 
                 <Button type="submit" class="mt-4 w-full" :disabled="form.processing">
                     <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                    Reset password
+                    {{ $t('Reset Password') }}
                 </Button>
             </div>
         </form>
