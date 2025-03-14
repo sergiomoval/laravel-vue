6fd9233b5aea2f35490c6d4671fa4d8527f63835
Sergio Moreno <sergiomorenoval@gmail.com>
2025-03-14 15:14:30 -0600

    feat: Agregado default theme light.

--- resources/js/composables/useAppearance.ts
+++ resources/js/composables/useAppearance.ts
@@ -21,7 +21,7 @@ const handleSystemThemeChange = () => {
 export function initializeTheme() {
     // Initialize theme from saved preference or default to system...
     const savedAppearance = localStorage.getItem('appearance') as Appearance | null;
-    updateTheme(savedAppearance || 'system');
+    updateTheme(savedAppearance || 'light');
 
     // Set up system theme change listener...
     mediaQuery.addEventListener('change', handleSystemThemeChange);
